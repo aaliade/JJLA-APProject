c699e293c15f0c23d2cbfc59fed595ff98d99c16
lasce <lasce@DESKTOP-C1U8S3G>
2023-11-16 00:55:03 -0500

    .

---
+++ CustomerClient/src/models/User.java
@@ -0,0 +1,176 @@
+package models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.DiscriminatorType;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.annotations.Columns;
+
+import factories.SessionFactoryBuilder;
+
+
+@Entity(name = "user")
+@Table(name = "user")
+//each subclass has its own table, and there is a table for the superclass containing common fields.
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "address")
+	private String address;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "firstName")
+	private String firstName;
+
+	@Column(name = "lastName")
+	private String lastName;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "phoneNum")
+	private String phone;
+
+	@Column(name = "userType")
+	private String userType;
+
+	@Id
+	@Column(name = "username")
+	private String username;
+
+	private static final Logger logger = LogManager.getLogger(User.class);
+
+	// Default Constructor
+	public User() {
+		this.username = "";
+		this.password = "";
+		this.firstName = "";
+		this.lastName = "";
+		this.phone = "";
+		this.email = "";
+		this.address = "";
+		this.userType = "";
+		logger.info("User initialized");
+	}
+
+	// Primary Constructor
+	public User(String username, String password, String firstName, String lastName, String phone, String email,
+			String address, String userType) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+		this.email = email;
+		this.address = address;
+		this.userType = userType;
+	}
+
+	// Copy Constructor
+	public User(User user) {
+		this.username = user.username;
+		this.password = user.password;
+		this.firstName = user.firstName;
+		this.lastName = user.lastName;
+		this.phone = user.phone;
+		this.email = user.email;
+		this.address = user.address;
+		this.userType = user.userType;
+		logger.info("User copied");
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public static Logger getLogger() {
+		return logger;
+	}
+
+	public abstract boolean login();
+
+}
---
+++ Server/src/models/Event.java
@@ -0,0 +1,239 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import factories.DBConnectorFactory;
+ 
+public class Event {
+	public static final Logger logger = LogManager.getLogger(Event.class);
+	private int eventID;
+	private String eventName;
+	private String eventDate;
+	private String eventLocation;
+	private Connection dbConn = null;
+	private Statement stmt = null;
+	private ResultSet result = null;
+	
+	public Event() {
+		eventID = 0;
+		eventName = "";
+		eventDate = "";
+		eventLocation = "";
+		logger.info("Event initialized");
+		this.dbConn = DBConnectorFactory.getDatabaseConnection();
+	}
+	
+	public Event(int eventID, String eventName, String eventDate, String eventLocation) {
+		this.eventID = eventID;
+		this.eventName = eventName;
+		this.eventDate = eventDate;
+		this.eventLocation = eventLocation;
+		logger.info("Input accepted, Event initialized");
+	}
+	
+	public int geteventID() {
+		logger.info("Event ID returned");
+		return eventID;
+	}
+	
+	public void seteventID(int eventID) {
+		this.eventID = eventID;
+		logger.info("Input accepted, Event ID set");
+	}
+	
+	public String geteventName() {
+		logger.info("Event Name returned");
+		return eventName;
+	}
+	
+	public void seteventName(String eventName) {
+		this.eventName = eventName;
+		logger.info("Input accepted, Event Name set");
+	}
+	
+	public String geteventDate() {
+		logger.info("Event Date returned");
+		return eventDate;
+	}
+	
+	public void seteventDate(String eventDate) {
+		this.eventDate = eventDate;
+		logger.info("Input accepted, Event Date set");
+	}
+	
+	public String geteventLocation() {
+		logger.info("Event Location returned");
+		return eventLocation;
+	}
+	
+	public void seteventLocation(String eventLocation) {
+		this.eventLocation = eventLocation;
+		logger.info("Input accepted, Event Location set");
+	}
+	
+	@Override
+	public String toString() {
+		logger.info("Event information returned");
+		return "Event ID: " + eventID + "Event Name: " + eventName + "Event Date: " + eventDate + "Event Location: " + eventLocation;
+	}
+    
+    public void selectAll() {
+        String sql = "SELECT * FROM grizzly’sentertainmentequipmentrental.event;";
+
+        try {
+            stmt = dbConn.createStatement();
+            result = stmt.executeQuery(sql);
+
+            while (result.next()) {
+                int eventID = result.getInt("eventID");
+                String eventName = result.getString("eventName");
+                String eventDate = result.getString("eventDate");
+                String eventLocation = result.getString("eventLocation");
+
+                System.out.println("Event ID: " + eventID + "\nEvent Name: " + eventName +
+                        "\nEvent Date: " + eventDate + "\nEvent Location: " + eventLocation + "\n");
+            }
+        } catch (SQLException e) {
+            System.err.println("SQL Exception: " + e.getMessage());
+            logger.error("SQL Exception while selecting events: " + e.getMessage());
+        } finally {
+            try {
+                stmt.close();
+                result.close();
+            } catch (SQLException e) {
+                System.err.println("Error while closing statement/result: " + e.getMessage());
+                logger.error("Error while closing statement/result: " + e.getMessage());
+            }
+        }
+    }
+
+    public void selectEventById(int eventID) {
+        String sql = "SELECT * FROM grizzly’sentertainmentequipmentrental.event WHERE eventID = " + eventID + ";";
+
+        try {
+            stmt = dbConn.createStatement();
+            result = stmt.executeQuery(sql);
+
+            while (result.next()) {
+                int id = result.getInt("eventID");
+                String eventName = result.getString("eventName");
+                String eventDate = result.getString("eventDate");
+                String eventLocation = result.getString("eventLocation");
+
+                System.out.println("Event ID: " + id + "\nEvent Name: " + eventName +
+                        "\nEvent Date: " + eventDate + "\nEvent Location: " + eventLocation + "\n");
+            }
+        } catch (SQLException e) {
+            System.err.println("SQL Exception: " + e.getMessage());
+            logger.error("SQL Exception while selecting events by ID: " + e.getMessage());
+        } finally {
+            try {
+                stmt.close();
+                result.close();
+            } catch (SQLException e) {
+                System.err.println("Error while closing statement/result: " + e.getMessage());
+                logger.error("Error while closing statement/result: " + e.getMessage());
+            }
+        }
+    }
+	
+	public void insert(int eventID, String eventName, String eventDate, String eventLocation) {
+		String sql = "INSERT INTO grizzly’sentertainmentequipmentrental.event (eventID, eventName, eventDate, eventLocation)" + "VALUES ('" + eventID + "', '"
+				+ eventName + "', '" + eventDate + "', '" + eventLocation + "');";
+
+	    try {
+	        stmt = dbConn.createStatement();
+
+	        int inserted = stmt.executeUpdate(sql);
+	        if (inserted == 1) {
+	            JOptionPane.showMessageDialog(null, "Event Record Inserted Successfully!", "Insertion Status", JOptionPane.INFORMATION_MESSAGE);
+	            logger.info("Event Record (ID: " + eventID + ") Inserted Successfully");
+	        } else {
+	            JOptionPane.showMessageDialog(null, "Event Record Insertion Failed.", "Insertion Status", JOptionPane.ERROR_MESSAGE);
+	            logger.error("Event Record (ID: " + eventID + ") Insertion Failed");
+	        }
+	    } catch (SQLException e) {
+	        System.err.println("SQL Exception: " + e.getMessage());
+	        logger.error("SQL Exception while inserting Event Record (ID: " + eventID + "): " + e.getMessage());
+	    } catch (Exception e) {
+	        System.err.println("Unexpected Error: " + e.getMessage());
+	        logger.error("Unexpected Error while inserting Event Record (ID: " + eventID + "): " + e.getMessage());
+	    } finally {
+	        try {
+	            stmt.close();
+	        } catch (SQLException e) {
+	            System.err.println("Error while closing statement: " + e.getMessage());
+	            logger.error("Error while closing statement: " + e.getMessage());
+	        }
+	    }
+	}
+
+	public void update(String UNDECIDED) {
+		String sql = "UPDATE grizzly’sentertainmentequipmentrental.event " + "SET --- = '" + UNDECIDED + "'" + " WHERE UNDECIDED = '" + UNDECIDED+ "'";
+
+		try {
+			stmt = dbConn.createStatement();
+			int updated = stmt.executeUpdate(sql);
+			
+	        if (updated == 1) {
+	            JOptionPane.showMessageDialog(null, "Event Record Updated Successfully!", "Update Status", JOptionPane.INFORMATION_MESSAGE);
+	            logger.info("Event Record (Column: " + UNDECIDED + ", Condition: " + UNDECIDED + ") Updated Successfully");
+	        } else {
+	            JOptionPane.showMessageDialog(null, "Event Record Update Failed.", "Update Status", JOptionPane.ERROR_MESSAGE);
+	            logger.error("Event Record (Column: " + UNDECIDED + ", Condition: " + UNDECIDED + ") Update Failed");
+	        }
+	    } catch (SQLException e) {
+	        System.err.println("SQL Exception: " + e.getMessage());
+	        logger.error("SQL Exception while updating Event Record (Column: " + UNDECIDED + ", Condition: " + UNDECIDED + "): " + e.getMessage());
+	    } catch (Exception e) {
+	        System.err.println("Unexpected Error: " + e.getMessage());
+	        logger.error("Unexpected Error while updating Event Record (Column: " + UNDECIDED + ", Condition: " + UNDECIDED + "): " + e.getMessage());
+	    } finally {
+	        try {
+	            stmt.close();
+	        } catch (SQLException e) {
+	            System.err.println("Error while closing statement: " + e.getMessage());
+	            logger.error("Error while closing statement: " + e.getMessage());
+	        }
+	    }
+	}
+
+	public void delete(int eventId) {
+		String sql = "DELETE FROM grizzly’sentertainmentequipmentrental.event WHERE eventID = " + eventId + ";";
+
+		try {
+			stmt = dbConn.createStatement();
+			int deleted = stmt.executeUpdate(sql);
+			if (deleted == 1) {
+				JOptionPane.showMessageDialog(null, "Event Record Deleted!", "Deletion Status",
+						JOptionPane.INFORMATION_MESSAGE);
+				logger.info("Event Record (ID: " + eventId + ") Deleted");
+			} else {
+				JOptionPane.showMessageDialog(null, "Event Record Deletion Failed.", "Deletion Status",
+						JOptionPane.ERROR_MESSAGE);
+				logger.error("Event Record (ID: " + eventId + ") Deletion Failed");
+			}
+		} catch (SQLException e) {
+			System.err.println("SQL Exception: " + e.getMessage());
+	        logger.error("SQL Exception while deleting Event Record (ID: " + eventId + "): " + e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Unexpected Error: " + e.getMessage());
+	        logger.error("Unexpected Error while deleting Event Record (ID: " + eventId + "): " + e.getMessage());
+		} finally {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				System.err.println("Error while closing statement: " + e.getMessage());	
+				logger.error("Error while closing statement: " + e.getMessage());
+			}
+		}
+	}
+}
