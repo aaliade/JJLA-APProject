ffbc846c2b1ac0b986c845385d10c5a8082ea88e
lasce <lasce@DESKTOP-C1U8S3G>
2023-11-16 13:59:49 -0500

    Push Customer

---
+++ CustomerClient/src/models/Employee.java
@@ -0,0 +1,213 @@
+
+package models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.annotations.Type;
+
+import factories.SessionFactoryBuilder;
+
+@Entity(name = "employee")
+@PrimaryKeyJoinColumn(name = "username")
+@Table(name = "employee")
+public class Employee extends User implements Serializable { // in order for the class to be sent across a network it
+																// needs to be serialized
+
+	private static final long serialVersionUID = 1L;
+
+	// no need to use @Id as the joined table is seen as a reference identifier
+	@Column(name = "empID")
+	private int empID;
+	@Column(name = "empRole")
+	private String empRole;
+	@Temporal(TemporalType.DATE) // set the type to match the data type in mysql
+	@Column(name = "hireDate")
+	private Date hireDate;
+
+	private static final Logger logger = LogManager.getLogger(Employee.class);
+
+	// Default Constructor
+	public Employee() {
+		super();
+		this.empID = 0;
+		this.empRole = "";
+		this.hireDate = null;
+		logger.info("Employee initialized");
+	}
+
+	// Primary Constructor
+	public Employee(int empID, String empRole, Date hireDate, String username, String password, String firstName,
+			String lastName, String phone, String email, String address, String usertype) {
+		super(username, password, firstName, lastName, phone, email, address, usertype);
+		this.empID = empID;
+		this.empRole = empRole;
+		this.hireDate = hireDate;
+		logger.info("Input accepted, Employee initialized");
+	}
+
+	// Primary Constructor 2
+	public Employee(int empID, String empRole, Date date) {
+		this.empID = empID;
+		this.empRole = empRole;
+		this.hireDate = date;
+		logger.info("Input accepted, Employee initialized");
+	}
+
+	// Copy Constructor
+	public Employee(Employee emp) {
+		super(emp.getUsername(), emp.getPassword(), emp.getFirstName(), emp.getLastName(), emp.getPhone(),
+				emp.getEmail(), emp.getAddress(), emp.getUserType());
+		this.empID = emp.empID;
+		this.empRole = emp.empRole;
+		this.hireDate = emp.hireDate;
+		logger.info("Employee copied");
+	}
+
+	public int getEmpID() {
+		logger.info("Employee ID returned");
+		return empID;
+	}
+
+	public void setEmpID(int empID) {
+		this.empID = empID;
+		logger.info("Input accepted, Employee ID set");
+	}
+
+	public String getEmpRole() {
+		logger.info("Employee Role returned");
+		return empRole;
+	}
+
+	public void setEmpRole(String empRole) {
+		this.empRole = empRole;
+	}
+
+	public Date getHireDate() {
+		logger.info("Employee Hire Date returned");
+		return hireDate;
+	}
+
+	public void setHireDate(Date date) {
+		this.hireDate = date;
+		logger.info("Input accepted, Employee Hire Date set");
+	}
+
+	public String getDateToString() {
+		return hireDate.toString();
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empID=" + empID + ", empRole=" + empRole + ", hireDate=" + hireDate + "]";
+	}
+
+	public boolean create() {
+		Session session = null;
+		Transaction transaction = null;
+//		Logger logger = LogManager.getLogger(getClass());
+		session = SessionFactoryBuilder.getEmployeeSessionFactroy().getCurrentSession();
+		transaction = session.beginTransaction();
+		session.save(this);
+		transaction.commit();
+		session.close();
+		return true;
+	}
+
+	public void update() {
+		Session session = null;
+		Transaction transaction = null;
+		Logger logger = LogManager.getLogger(getClass());
+
+		try {
+			session = SessionFactoryBuilder.getEmployeeSessionFactroy().getCurrentSession();
+			transaction = session.beginTransaction();
+			Employee emp = (Employee) session.get(Employee.class, this.empID);
+			emp.setEmpID(empID);
+			emp.setUsername(emp.getUsername());
+			emp.setEmpRole(empRole);
+			emp.setHireDate(hireDate);
+
+			emp.setFirstName(emp.getFirstName());
+			emp.setLastName(emp.getLastName());
+			emp.setAddress(emp.getAddress());
+			emp.setEmail(emp.getEmail());
+			emp.setPassword(emp.getPassword());
+			emp.setPhone(emp.getPhone());
+
+			session.update(emp);
+			transaction.commit();
+		} catch (HibernateException e) {
+			// Log and handle HibernateException
+			logger.error("Error occurred while updating employee", e);
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+		} catch (Exception e) {
+			// Log and handle other exceptions
+			logger.error("Error occurred while updating employee", e);
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+		} finally {
+			if (session != null && session.isOpen()) {
+				session.close();
+			}
+		}
+	}
+	
+	public Employee findEmployee(String username) {
+		 Session session = SessionFactoryBuilder.getEmployeeSessionFactroy().getCurrentSession();
+		 Transaction transaction = session.beginTransaction();
+		 Employee employee = null;
+		 try {
+			 employee = session.get(Employee.class, username);
+			 transaction.commit();
+			 session.close();
+			 return employee;
+		 }catch (HibernateException e) {
+		        // Log and handle HibernateException
+		        logger.error("Error occurred while searching for customer", e);
+		        if (transaction != null && transaction.isActive()) {
+		            transaction.rollback();
+		        }
+		    } catch (Exception e) {
+		        // Log and handle other exceptions
+		        logger.error("Error occurred while searching for customer", e);
+		        if (transaction != null && transaction.isActive()) {
+		            transaction.rollback();
+		        }
+		    } finally {
+		        if (session != null && session.isOpen()) {
+		            session.close();
+		        }
+		    }
+		 return employee;
+	}
+
+	@Override
+	public boolean login() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ CustomerClient/src/models/Equipment.java
@@ -0,0 +1,281 @@
+package models;
+
+import org.apache.logging.log4j.Logger;
+
+import factories.DBConnectorFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+import org.apache.logging.log4j.LogManager;
+
+
+public class Equipment{
+	private static final Logger logger = LogManager.getLogger(Equipment.class);
+	private String categoryName;
+	private int equipID;
+	private String equipName;
+	private String description;
+	private boolean status;
+	private String category;
+	private double rentalRate;
+	private Connection dbConn = null;
+	private Statement stmt = null;
+	private ResultSet result = null;
+
+	public Equipment() {
+		equipID = 0;
+		equipName = "";
+		description = "";
+		status = true;
+		category = "";
+		rentalRate = 0;
+		logger.info("Equipment initialized");
+		this.dbConn = DBConnectorFactory.getDatabaseConnection();
+	}
+	
+
+	public Equipment(int equipID, String equipName, String description, boolean status, String category, double rentalRate) {
+		this.equipID = equipID;
+		this.equipName = equipName;
+		this.description = description;
+		this.status = status;
+		this.category = category;
+		this.rentalRate = rentalRate;
+		logger.info("Input accepted, Equipment initialized");
+	}
+
+	
+	public String getcategoryName() {
+		logger.info("Equipment Category Name returned");
+		return categoryName;
+	}
+	
+	public void setcategoryName(String categoryName) {
+		this.categoryName = categoryName;
+		logger.info("Input accepted, Category Name set");
+	}
+	
+	public int getequipID() {
+		logger.info("Equipment ID returned");
+		return equipID;
+	}
+	
+	public void setequipID(int equipID) {
+		this.equipID = equipID;
+		logger.info("Input accepted, Equipment ID set");
+	}
+	
+	public String getequipName() {
+		logger.info("Equipment Name returned");
+		return equipName;
+	}
+	
+	public void setequipName(String equipName) {
+		this.equipName = equipName;
+		logger.info("Input accepted, Equipment Name set");
+	}
+	
+	public String getdescription() {
+		logger.info("Equipment Description returned");
+		return description;
+	}
+	
+	public void setdescription(String description) {
+		this.description = description;
+		logger.info("Input accepted, Equipment Description set");
+	}
+
+	public boolean getstatus() {
+		logger.info("Equipment Status returned");
+		return status;
+	}
+
+	public void setstatus(boolean status) {
+		this.status = status;
+		logger.info("Input accepted, Equipment Status set");
+	}
+	
+	public String getCategory() {
+		logger.info("Equipment Status returned");
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+		logger.info("Input accepted, Equipment Status set");
+	}
+
+	public double getrentalRate() {
+		logger.info("Equipment Rental Rate returned");
+		return rentalRate;
+	}
+
+	public void setrentalRate(double rentalRate) {
+		this.rentalRate = rentalRate;
+		logger.info("Input accepted, Equipment Rental Rate set");
+	}
+	
+	@Override
+	public String toString() {
+		logger.info("Equipment information returned");
+		return "Equipment ID" + equipID + "Equipment Name" + equipName + "Description" + description + "Status" + status + "Rental Rate" + rentalRate;	
+	}
+	
+	public void selectAll() {
+        String sql = "SELECT * FROM grizzly’sentertainmentequipmentrental.equipment;";
+
+        try {
+            stmt = dbConn.createStatement();
+            result = stmt.executeQuery(sql);
+
+            while (result.next()) {
+                int equipID = result.getInt("equipID");
+                String equipName = result.getString("equipName");
+                String description = result.getString("description");
+                String category = result.getString("catgeory");
+                boolean status = result.getBoolean("status");
+                double rentalRate = result.getInt("rentalRate");
+
+                System.out.println("Equipment ID: " + equipID + "\nEquipment Name: " + equipName +
+                        "\nDescription: " + description + "\nStatus: " + status + "\nCategory: " + category + "\nRental Rate: " + rentalRate + "\n");
+            }
+        } catch (SQLException e) {
+            System.err.println("SQL Exception: " + e.getMessage());
+            logger.error("SQL Exception while selecting equipments: " + e.getMessage());
+        } finally {
+            try {
+                stmt.close();
+                result.close();
+            } catch (SQLException e) {
+                System.err.println("Error while closing statement/result: " + e.getMessage());
+                logger.error("Error while closing statement/result: " + e.getMessage());
+            }
+        }
+    }
+
+	public void selectAvailableEquipmentByCategory(String category) {
+	    String sql = "SELECT * FROM grizzly’sentertainmentequipmentrental.equipment WHERE category = '" + category + "' AND status = 'Available';";
+
+	    try {
+	        stmt = dbConn.createStatement();
+	        result = stmt.executeQuery(sql);
+
+	        while (result.next()) {
+                int equipID = result.getInt("equipID");
+                String equipName = result.getString("equipName");
+                String description = result.getString("description");
+                boolean status = result.getBoolean("status");
+                String category1 = result.getString("catgeory");
+                double rentalRate = result.getInt("rentalRate");
+
+                System.out.println("Equipment ID: " + equipID + "\nEquipment Name: " + equipName +
+                        "\nDescription: " + description + "\nStatus: " + status + "\nCategory: " + category1 + "\nRental Rate: " + rentalRate + "\n");
+            }
+	    } catch (SQLException e) {
+	        System.err.println("SQL Exception: " + e.getMessage());
+	        logger.error("SQL Exception while selecting available equipment: " + e.getMessage());
+	    } finally {
+	        try {
+	            stmt.close();
+	            result.close();
+	        } catch (SQLException e) {
+	            System.err.println("Error while closing statement/result: " + e.getMessage());
+	            logger.error("Error while closing statement/result: " + e.getMessage());
+	        }
+	    }
+	}
+	
+	public void insert(int equipID, String equipName, String description, boolean status, String category, int rentalRate) {
+		String sql = "INSERT INTO grizzly’sentertainmentequipmentrental.equipment (equipID, equipName, description, status, category, rentalRate)"
+	            + "VALUES ('" + equipID + "', '" + equipName + "', '" + description + "', '" + status + "', '" + category + "', '" + rentalRate + "');";
+
+        try {
+            stmt = dbConn.createStatement();
+
+            int inserted = stmt.executeUpdate(sql);
+            if (inserted == 1) {
+                System.out.println("Equipment Record Inserted Successfully!");
+                logger.info("Equipment Record (ID: " + equipID + ") Inserted Successfully");
+            } else {
+                System.out.println("Equipment Record Insertion Failed.");
+                logger.error("Equipment Record (ID: " + equipID + ") Insertion Failed");
+            }
+        } catch (SQLException e) {
+            System.err.println("SQL Exception: " + e.getMessage());
+            logger.error("SQL Exception while inserting Equipment Record (ID: " + equipID + "): " + e.getMessage());
+        } finally {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                System.err.println("Error while closing statement: " + e.getMessage());
+                logger.error("Error while closing statement: " + e.getMessage());
+            }
+        }
+    }
+
+	public void update(String UNDECIDED) {
+		String sql = "UPDATE grizzly’sentertainmentequipmentrental.equipment " + "SET --- = '" + UNDECIDED + "'" + " WHERE UNDECIDED = '" + UNDECIDED+ "'";
+
+		try {
+			stmt = dbConn.createStatement();
+			int updated = stmt.executeUpdate(sql);
+			
+	        if (updated == 1) {
+	            JOptionPane.showMessageDialog(null, "Equipment Record Updated Successfully!", "Update Status", JOptionPane.INFORMATION_MESSAGE);
+	            logger.info("Equipment Record (Column: " + UNDECIDED + ", Condition: " + UNDECIDED + ") Updated Successfully");
+	        } else {
+	            JOptionPane.showMessageDialog(null, "Equipment Record Update Failed.", "Update Status", JOptionPane.ERROR_MESSAGE);
+	            logger.error("Equipment Record (Column: " + UNDECIDED + ", Condition: " + UNDECIDED + ") Update Failed");
+	        }
+	    } catch (SQLException e) {
+	        System.err.println("SQL Exception: " + e.getMessage());
+	        logger.error("SQL Exception while updating Event Record (Column: " + UNDECIDED + ", Condition: " + UNDECIDED + "): " + e.getMessage());
+	    } catch (Exception e) {
+	        System.err.println("Unexpected Error: " + e.getMessage());
+	        logger.error("Unexpected Error while updating Event Record (Column: " + UNDECIDED + ", Condition: " + UNDECIDED + "): " + e.getMessage());
+	    } finally {
+	        try {
+	            stmt.close();
+	        } catch (SQLException e) {
+	            System.err.println("Error while closing statement: " + e.getMessage());
+	            logger.error("Error while closing statement: " + e.getMessage());
+	        }
+	    }
+	}
+
+	public void delete(int equipId) {
+		String sql = "DELETE FROM grizzly’sentertainmentequipmentrental.equipment WHERE equipID = " + equipId + ";";
+
+		try {
+			stmt = dbConn.createStatement();
+			int deleted = stmt.executeUpdate(sql);
+			if (deleted == 1) {
+				JOptionPane.showMessageDialog(null, "Equipment Record Deleted!", "Deletion Status",
+						JOptionPane.INFORMATION_MESSAGE);
+				logger.info("Equipment Record (ID: " + equipId + ") Deleted");
+			} else {
+				JOptionPane.showMessageDialog(null, "Equipment Record Deletion Failed.", "Deletion Status",
+						JOptionPane.ERROR_MESSAGE);
+				logger.error("Equipment Record (ID: " + equipId + ") Deletion Failed");
+			}
+		} catch (SQLException e) {
+			System.err.println("SQL Exception: " + e.getMessage());
+	        logger.error("SQL Exception while deleting Event Record (ID: " + equipId + "): " + e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Unexpected Error: " + e.getMessage());
+	        logger.error("Unexpected Error while deleting Event Record (ID: " + equipId + "): " + e.getMessage());
+		} finally {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				System.err.println("Error while closing statement: " + e.getMessage());	
+				logger.error("Error while closing statement: " + e.getMessage());
+			}
+		}
+	}	
+}
